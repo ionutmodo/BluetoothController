34000d5631cf759fcad05f2579c72e633308fa56
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2018-08-19 15:32:34 +0300

    changed command values to more suggestive numbers

--- app/src/main/java/com/home/bluetoothcontroller/Settings.java
+++ app/src/main/java/com/home/bluetoothcontroller/Settings.java
@@ -6,21 +6,25 @@ package com.home.bluetoothcontroller;
 
 public class Settings
 {
-    public static final char FORWARD = 'F';
-    public static final char BACKWARD = 'B';
-    public static final char LEFT = 'L';
-    public static final char RIGHT = 'R';
-    public static final char FORWARD_LEFT = 'G';
-    public static final char FORWARD_RIGHT = 'I';
-    public static final char BACK_LEFT = 'H';
-    public static final char BACK_RIGHT = 'J';
-    public static final char STOP = 'S';
-    public static final char FRONT_LIGHTS_ON = 'W';
-    public static final char FRONT_LIGHTS_OFF = 'w';
-    public static final char BACK_LIGHTS_ON = 'U';
-    public static final char BACK_LIGHTS_OFF = 'u';
-    public static final char HORN_ON = 'V';
-    public static final char HORN_OFF = 'v';
-    public static final char[] SPEED = new char[] {'0', '1','2','3','4','5','6','7','8','9','q'};
-    public static final char STOP_ALL = 'D';
+    // SPEED: 0...10
+    public static final byte FORWARD_LEFT = 11;
+    public static final byte FORWARD = 12;
+    public static final byte FORWARD_RIGHT = 13;
+
+    public static final byte LEFT = 21;
+    public static final byte STOP = 22;
+    public static final byte RIGHT = 23;
+
+    public static final byte BACK_LEFT = 31;
+    public static final byte BACKWARD = 32;
+    public static final byte BACK_RIGHT = 33;
+
+    public static final byte FRONT_LIGHTS_OFF = 40;
+    public static final byte FRONT_LIGHTS_ON = 41;
+
+    public static final byte BACK_LIGHTS_OFF = 50;
+    public static final byte BACK_LIGHTS_ON = 51;
+
+    public static final byte HORN_OFF = 60;
+    public static final byte HORN_ON = 61;
 }
